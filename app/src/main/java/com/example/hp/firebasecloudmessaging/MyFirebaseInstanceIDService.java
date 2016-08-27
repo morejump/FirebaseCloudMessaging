57aa835b9fccfffa641fdde72bd37b0c95e76869
DESKTOP-V5C0MBO\HP <luuvanthaoepu@gmail.com>
2016-08-27 14:16:00 +0700

    muốn get sha-1 thì vào left-pane ---->refesh---> task-->report

--- app/src/main/java/com/example/hp/firebasecloudmessaging/MyFirebaseInstanceIDService.java
+++ app/src/main/java/com/example/hp/firebasecloudmessaging/MyFirebaseInstanceIDService.java
@@ -9,6 +9,7 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
  * Created by HP on 8/27/2016.
  */
 public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+    //
     @Override
     public void onTokenRefresh() {
         super.onTokenRefresh();
